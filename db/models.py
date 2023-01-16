d613d7195532a7787b698cfba329164000d56eef
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-01-16 13:10:05 +0200

    Have corrected main queries and add another two from additional part

--- db/models.py
+++ db/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey, Table
+from sqlalchemy.sql.schema import ForeignKey
 
 
 Base = declarative_base()
@@ -11,6 +11,7 @@ class Class(Base):
     __tablename__ = "classes"
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
+
     students = relationship("Student", back_populates="group")
 
 
@@ -19,15 +20,16 @@ class Student(Base):
     id = Column(Integer, primary_key=True)
     fullname = Column(String(150), nullable=False)
     class_id = Column(Integer, ForeignKey('classes.id', ondelete="CASCADE"))
+
     group = relationship('Class', back_populates='students')
     grades = relationship('Grade', back_populates='student')
-    
 
 
 class Teacher(Base):
     __tablename__ = "teachers"
     id = Column(Integer, primary_key=True)
     fullname = Column(String(150), nullable=False)
+
     subjects = relationship("Subject", back_populates="teacher")
 
 
@@ -36,6 +38,7 @@ class Subject(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False, unique=True)
     teacher_id = Column(Integer, ForeignKey('teachers.id', ondelete="CASCADE"))
+
     teacher = relationship('Teacher', back_populates='subjects')
     grades = relationship('Grade', back_populates='subject')
 
@@ -47,18 +50,6 @@ class Grade(Base):
     date_of = Column(Date, nullable=False)
     subject_id = Column(Integer, ForeignKey('subjects.id', ondelete="CASCADE"))
     student_id = Column(Integer, ForeignKey('students.id', ondelete="CASCADE"))
+
     subject = relationship('Subject', back_populates='grades')
     student = relationship('Student', back_populates='grades')
-
-
-
-
-# should be m2m
-# class GradeStudent(Base):
-#     __tablename__ = "grades_to_students"
-#     id = Column(Integer, primary_key=True)
-#     class_id = Column('class_id', ForeignKey('classes.id', ondelete='CASCADE'))
-#     student_id = Column('student_id', ForeignKey(
-#         'students.id', ondelete='CASCADE'))
-
-
--- queries/my_select.py
+++ queries/my_select.py
@@ -5,25 +5,21 @@ sys.path.append(os.getcwd())
 from db.models import Class, Student, Teacher, Subject, Grade
 from db.connect_db import session
 
-from sqlalchemy import select, func, desc
+from sqlalchemy import func, desc, and_
 
 
 
 def query_1():
-    q = session.query(Student.fullname, func.round(func.avg(Grade.grade), 2).label('avg_rate'))\
-        .select_from(Student).join(Grade).filter(Grade.student_id == Student.id).group_by(Student.id).order_by(desc('avg_rate')).limit(5).all()
-    return q
-
+    return session.query(Student.fullname, func.round(func.avg(Grade.grade), 2).label('avg_rate')).select_from(Student).join(
+        Grade).filter(Grade.student_id == Student.id).group_by(Student.id).order_by(desc('avg_rate')).limit(5).all()
 
 def query_2():
-    q = session.query(Student.fullname, Subject.name, Grade.grade).select_from(Grade).join(Student, Subject).filter(
-        Student.id == Grade.student_id and Subject.id == Grade.subject_id).filter(Subject.id == 7).order_by(desc(Grade.grade)).limit(1).all()
-    return q
-
+    return session.query(Student.fullname, Subject.name, Grade.grade).select_from(Grade).join(Student, Subject).filter(
+        and_(Student.id == Grade.student_id, Subject.id == Grade.subject_id, Subject.id == 7)).order_by(desc(Grade.grade)).limit(1).all()
+    
 def query_3():
-    return session.query(Class.name, func.round(func.avg(Grade.grade), 2)).select_from(Grade).join(Student, Subject, Class).filter(
-        Grade.student_id == Student.id and Subject.id == Grade.subject_id and Class.id == Student.class_id).filter(Subject.id == 3).group_by(
-            Class.name).order_by(Class.name).all()
+    return session.query(Class.name, func.round(func.avg(Grade.grade), 2)).select_from(Grade).join(Student, Subject, Class).filter(and_(Grade.student_id == 
+    Student.id, Subject.id == Grade.subject_id, Class.id == Student.class_id, Subject.id == 3)).group_by(Class.name).order_by(Class.name).all()
 
 def query_4():
     return session.query(func.round(func.avg(Grade.grade), 2)).select_from(Grade).all()
@@ -32,24 +28,36 @@ def query_5():
     return session.query(Teacher.fullname, Subject.name).select_from(Subject).join(Teacher).filter(Subject.teacher_id == Teacher.id).order_by(Teacher.fullname).all()
 
 def query_6():
-    return session.query(Class.name, Student.fullname).select_from(Student).join(Class).filter(Student.class_id == Class.id).filter(Class.id == 1).order_by(Student.fullname).all()
+    return session.query(Class.name, Student.fullname).select_from(Student).join(Class).filter(and_(Student.class_id == Class.id, Class.id == 1)).order_by(
+        Student.fullname).all()
 
 def query_7():
-    return session.query(Class.name, Student.fullname, Subject.name, Grade.grade).select_from(Grade).join(Student, Class, Subject).filter(
-        Student.class_id == Class.id and Student.id == Grade.student_id and Subject.id == Grade.subject_id).filter(Class.id == 3).filter(Subject.id == 4).order_by(desc(Grade.grade)).all()
+    return session.query(Class.name, Student.fullname, Subject.name, Grade.grade).select_from(Grade).join(Student, Class, Subject).filter(and_(Student.class_id == Class.id,
+     Student.id == Grade.student_id, Subject.id == Grade.subject_id)).filter(Class.id == 3).filter(Subject.id == 4).order_by(desc(Grade.grade)).all()
 
 def query_8(): # some problems
-    return session.query(Teacher.fullname, Subject.name, func.round(func.avg(Grade.grade), 2)).select_from(Grade).join(Teacher, Subject).filter(Teacher.id == Subject.teacher_id and
-     Grade.subject_id == Subject.id).filter(Teacher.id == 5).group_by(Subject.name).all()
+    return session.query(Teacher.fullname, Subject.name, func.round(func.avg(Grade.grade), 2)).select_from(Grade).join(Teacher, Subject).filter(and_(
+        Teacher.id == Subject.teacher_id, Grade.subject_id == Subject.id, Teacher.id == 5)).group_by(Subject.name).all()
 
 def query_9(): # .distinct()
-    return session.query(Student.fullname, Subject.name).select_from(Student).join(Grade, Subject).filter(Student.id == Grade.student_id and Subject.id == Grade.subject_id).filter(Student.id == 50).order_by(Subject.name)
+    return session.query(Student.fullname, Subject.name).select_from(Student).join(Grade, Subject).filter(and_(Student.id == Grade.student_id, 
+    Subject.id == Grade.subject_id, Student.id == 50)).order_by(Subject.name)
     
 
 def query_10():
-    return session.query(Student.fullname, Teacher.fullname, Subject.name).select_from(Grade).join(Subject, Student, Teacher).filter(Subject.id == Grade.subject_id and Student.id == Grade.student_id 
-    and Teacher.id == Subject.teacher_id).filter(Student.id == 42).filter(Teacher.id == 3).order_by(Subject.name).all()
+    return session.query(Student.fullname, Teacher.fullname, Subject.name).select_from(Grade).join(Subject, Student, Teacher).filter(and_(Subject.id == Grade.subject_id,
+     Student.id == Grade.student_id, Teacher.id == Subject.teacher_id, Student.id == 42, Teacher.id == 3)).order_by(Subject.name).all()
 
-    
-for r in query_10():
+def query_11():
+    return session.query(Teacher.fullname, Student.fullname, func.round(func.avg(Grade.grade), 2)).select_from(Grade).join(Subject, Student, Teacher).filter(and_(
+        Subject.id == Grade.subject_id, Student.id == Grade.student_id, Teacher.id == Subject.teacher_id, Student.id == 2, Teacher.id == 2)).group_by(Teacher.fullname, Student.fullname).all()
+
+def query_12():
+    subq = session.query(func.max(Grade.date_of)).select_from(Grade).join(Student, Class, Subject).filter(and_(Grade.student_id == Student.id,
+     Student.class_id == Class.id, Subject.id == Grade.subject_id, Class.id == 2, Subject.id == 1)).one()
+
+    return session.query(Class.name, Student.fullname, Subject.name, Grade.grade, Grade.date_of).select_from(Grade, Student, Class, Subject).filter(and_(Grade.student_id == Student.id,
+     Student.class_id == Class.id, Subject.id == Grade.subject_id, Class.id == 2, Subject.id == 1, Grade.date_of.in_(subq))).all()
+
+for r in query_12():
     print(r)
